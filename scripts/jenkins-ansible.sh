374ce9442a7f513491e2f6f64e511380543f55b8
haquocdat543 <wwwdatha543@gmail.com>
2023-12-10 02:18:03 +0000

    Update script

--- scripts/jenkins-ansible.sh
+++ scripts/jenkins-ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-username="haquocdat"
-reponame="jenkins-ansible"
+USERNAME="haquocdat543"
+REPONAME="jenkins-ansible"
 exec > >(tee -i /var/log/user-data.log)
 exec 2>&1
 sudo apt update -y
@@ -11,8 +11,8 @@ sudo apt install ansible -y
 sudo apt install git -y 
 mkdir Ansible && cd Ansible
 pwd
-git clone https://github.com/${username}/${reponame}.git   #change to your own repo
-cd ${reponame}
+git clone https://github.com/${USERNAME}/${REPONAME}.git   #change to your own repo
+cd ${REPONAME}
 ansible-playbook -i localhost Jenkins-playbook.yml
 
 hostnamectl set-hostname Jenkins-Ansible 
